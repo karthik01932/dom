e7fdb501129392cfdbb586fad7034ba706c66c11
karthik01932 <karthiksrinivas2001>
2023-10-02 08:53:05 +0530

    background changes

--- dom.js
+++ dom.js
@@ -28,6 +28,12 @@ subtitle.style.fontWeight =  'bold';
 
 
 // GET ELEMENT BY CLASS NAME
-// var items = document.getElementsByClassName('list-group-item');
+var items = document.getElementsByClassName('list-group-item');
 // console.log(items);
 // console.log(items[2]);
+items[2].style.backgroundColor = 'green';
+items[1].style.fontWeight = 'bold';
+items[2].style.fontWeight = 'bold';
+items[3].style.fontWeight = 'bold';
+items[0].style.fontWeight = 'bold';
+
